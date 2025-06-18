cda23d67daf04923a9039578c5adddb5cda112e3
EcoFercho <erwin0pisis@gmail.com>
2025-06-17 23:16:19 -0400

    javaScript variables y concatenaciones, backtick

---
+++ JavaScript/Class1Variables/code.js
@@ -0,0 +1,14 @@
+// variavles -> let se puede reasignar
+let variable = "hola"
+console.log(variable)
+console.log(typeof variable)
+
+//variables -> const no se puede reasignar
+const varConstante = "valor de PI";
+console.log(varConstante);
+console.log(typeof varConstante);
+
+// variables -> var se puede reasignar es una variable global
+var varGlobal = "variable global";
+console.log(varGlobal);
+console.log(typeof varGlobal);
---
+++ JavaScript/Class2Backtick/code.js
@@ -0,0 +1,13 @@
+
+// Tipos de concatenacion en variables
+let name = "andres Pedro";
+let age = 20;
+
+// concatenacion simpple 
+let datos = "Mi nombre es: " + name + " y mi edad es: " + age;
+console.log(datos);
+
+// Concatenacion con backtick
+console.log(`Mi nombre es ${name} y mi edad es ${age}`);
+console.log(typeof name);
+console.log(typeof age);
